8ba1df54002623a103dde76c4ed4a0dd5ab3b559
Unknown <oabbott@mail.csuchico.edu>
2017-05-09 13:47:21 -0700

    5/9/17 1:47

---
+++ java.js
@@ -0,0 +1,13 @@
+function puzzle1()
+{
+  var answer;
+  answer = document.getElementById("puzzle1Answer").value;
+  if(answer == "It operates on batteries" || answer == "it operates on batteries")
+  {
+    alert("That is correct!");
+  }
+  else
+  {
+    alert("Incorrect Answer; Try Again");
+  }
+}
