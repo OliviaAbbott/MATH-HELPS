6d68e967afa15daf8a5695487167fe5f3cf17e8d
Unknown <oabbott@mail.csuchico.edu>
2017-05-10 10:44:42 -0700

    5/10/17 10:44

--- java.js
+++ java.js
@@ -1,13 +1,60 @@
-function puzzle1()
+function checkPuzzle1()
 {
   var answer;
   answer = document.getElementById("puzzle1Answer").value;
   if(answer == "It operates on batteries" || answer == "it operates on batteries")
   {
-    alert("That is correct!");
+    document.getElementById(i).style.background = 'LightGreen';
   }
   else
   {
     alert("Incorrect Answer; Try Again");
   }
+}
+
+function checkPuzzle2()
+{
+  for(var i = 1; i <= 20; i++)
+  {
+    var correctAnswer = ["H", "E", "W", "A", "S", "P", "R", "E", "S", "S", "I", "N", "G", "H", "I", "S", "L", "U", "C", "K"];
+    var answer = document.getElementById(i).value;
+    if(answer == correctAnswer[i - 1])
+    {
+        document.getElementById(i).style.background = 'LightGreen';
+    }
+    else
+    {
+      document.getElementById(i).style.background = 'LightCoral';
+    }
+  }
+}
+
+function start()
+{
+
+}
+
+function checkProblem1()
+{
+
+}
+
+function checkProblem2()
+{
+
+}
+
+function checkProblem3()
+{
+
+}
+
+function checkProblem4()
+{
+
+}
+
+function checkProblem5()
+{
+
 }
