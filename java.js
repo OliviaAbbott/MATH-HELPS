bb73ca6c4bc643ccb95169b5058e5d0a299b95e3
Unknown <oabbott@mail.csuchico.edu>
2017-05-10 15:12:32 -0700

    5/10/17 3:12

--- java.js
+++ java.js
@@ -29,13 +29,44 @@ function checkPuzzle2()
   }
 }
 
+//courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+//courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
 function start()
 {
-
+  document.getElementById("problem_1").removeAttribute("hidden");
 }
 
 function checkProblem1()
 {
+  var a = document.getElementById("problem1a").value;
+  setCookie("problem1a", a, 10);
+  var b = document.getElementById("problem1b").value;
+  setCookie("problem1b", b, 10);
+  var c = document.getElementById("problem1c").value;
+  setCookie("problem1a", c, 10);
+  document.getElementById("problem_2").removeAttribute("hidden");
 
 }
 
@@ -55,6 +86,21 @@ function checkProblem4()
 }
 
 function checkProblem5()
+{
+
+}
+
+function checkProblem6()
+{
+
+}
+
+function checkProblem7()
+{
+
+}
+
+function checkProblem8()
 {
 
 }
